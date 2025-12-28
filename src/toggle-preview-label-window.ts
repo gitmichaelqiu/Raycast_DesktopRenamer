851b056fd066c93a5370f3450ef59b6e0eb8f3c1
Michael Y. Qiu <altmickymike@outlook.com>
2025-12-28 17:35:19 +0800

    Add toggle-preview-label-windows

---
+++ src/toggle-preview-label-window.ts
@@ -0,0 +1,11 @@
+import { showHUD } from "@raycast/api";
+import { runAppleScript } from "@raycast/utils";
+
+export default async function Command() {
+  try {
+    await runAppleScript(`tell application "DesktopRenamer" to toggle preview label`);
+    await showHUD("Toggled Preview Label Window");
+  } catch (error) {
+    await showHUD("Failed to toggle preview label.");
+  }
+}
