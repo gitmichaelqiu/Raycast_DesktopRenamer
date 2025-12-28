80b3f20703acef4ffd1480e93333d7d7b682167a
Michael Y. Qiu <altmickymike@outlook.com>
2025-12-28 17:35:49 +0800

    Add toggle-visibility-of-active-label-windows

---
+++ src/toggle-visibility-of-active-label-window.ts
@@ -0,0 +1,11 @@
+import { showHUD } from "@raycast/api";
+import { runAppleScript } from "@raycast/utils";
+
+export default async function Command() {
+  try {
+    await runAppleScript(`tell application "DesktopRenamer" to toggle desktop visibility`);
+    await showHUD("Toggled Desktop Visibility");
+  } catch (error) {
+    await showHUD("Failed to toggle desktop visibility.");
+  }
+}
