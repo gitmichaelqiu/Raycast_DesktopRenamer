c5fa798a1f7591eb68d196d350d19fafb94674b8
Michael Y. Qiu <altmickymike@outlook.com>
2026-01-20 10:42:03 +0800

    Remove unused components in current-desktop

--- src/current-desktop.tsx
+++ src/current-desktop.tsx
@@ -3,8 +3,6 @@ import { runAppleScript } from "@raycast/utils";
 
 export default async function Command() {
   try {
-    // According to DesktopRenamer.sdef, the command is "get current space name"
-    // It returns a text string.
     const script = `
       try
         tell application "DesktopRenamer" to get current space name
@@ -24,12 +22,6 @@ export default async function Command() {
     }
 
     const name = result.trim();
-    
-    // Calculate time ago (approximate since we just ran it)
-    const timeString = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });
-
-    // Update metadata. 
-    // Since we only get the name from the SDEF command, we display just the name.
     await updateCommandMetadata({
       subtitle: `${name}`,
     });
